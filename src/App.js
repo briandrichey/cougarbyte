fad9e161d30a7f760ecbbc71c8bb8d5e938cd9d3
brichey <brichey@isafeventures.com>
2023-02-19 20:03:34 -0800

    test script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Cougar Byte Project
+          Welcome to the Cougar Byte Tennibot Project
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
