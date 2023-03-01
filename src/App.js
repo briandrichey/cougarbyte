a10e3a57940d7bfd7a6c78f1dd978a62d878f1eb
brichey <brichey@isafeventures.com>
2023-03-01 13:27:26 -0800

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the CougarByte Tennibot Project! 
+          Welcome to the CougarByte Tennibot Project!
         </p>
 
         <p>
