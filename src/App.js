0fb421a9c3a4a8069725b969beeeb435b36a1f64
Jacob Vanderhorst <jacob@ad.oldran.ch>
2023-03-06 12:25:47 -0800

    Merged Upload & Cursor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import ImageUploader from './ImageUploader';
 
 function App() {
+  // Image Handling
+  const [userImage, setuserImage] = useState(null);
+  const imageUpload = (e) => {
+    document.getElementById('uploadControl').classList.add('hidden');
+    document.getElementById('imgContainer').classList.remove('hidden');
+    setuserImage(URL.createObjectURL(e.target.files[0]));
+  }
+
+  // Cursor Handling
+  const handleImageHover = (e) => {
+    let cursor = document.getElementById('cursor');
+    document.addEventListener('mousemove', function(e){
+      let x = e.clientX;
+      let y = e.clientY;
+      cursor.style.left = x + "px";
+      cursor.style.top = y + "px";
+    })
+    e.target.style.cursor = 'none';
+    //e.target.style.cursor = 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 16 16\' fill=\'white\'%3E%3Ccircle cx=\'5\' cy=\'5\' r=\'4\'/%3E%3C/svg%3E") 8 8, auto';
+  };
+
+  const handleImageLeave = (e) => {
+    e.target.style.cursor = 'auto';
+  };
+
+
+  // HTML Code
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id='uploadControl' className='mx-auto text-center'>
+        <label>Upload Image</label>
+        <br />
+        <input type="file" onChange={imageUpload} />
+      </div>
+      <div id = "cursor"></div>
+      <div id="imgContainer" className='m-2 text-center hidden' >
+        {userImage && (<img className='mx-auto' src={userImage} alt="Upload" onMouseOver={handleImageHover} 
+        onMouseLeave={handleImageLeave} />)}
         <p>
-          Welcome to the Blue Team Tennibot Project!
+          <label>Circle 1</label>
+          <input type="range" min="1" max="100" class="slider" id="sliderOne" />
         </p>
         <ImageUploader></ImageUploader>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>Circle 2</label>
+          <input type="range" min="1" max="100" class="slider" id="sliderTwo" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
