b8d850dfa24636771408c7765e690d1a9a2a48fb
Kristina <ktabar9@gmail.com>
2023-03-10 13:48:47 -0800

    test commit2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the CougarByte Tennibot Project!
+          Welcome to the CougarByte CB Project!
         </p>
         <ImageUploader></ImageUploader>
         <p>
